004749b429f38d442ad07308ca3ec3b49d075af4
Sriram Panyam <sri.panyam@gmail.com>
2016-12-03 23:00:11 -0800

    Woooo hooo all exercises complete

--- machine-learning/ex8/cofiCostFunc.m
+++ machine-learning/ex8/cofiCostFunc.m
@@ -13,49 +13,14 @@ Theta = reshape(params(num_movies*num_features+1:end), ...
 
             
 % You need to return the following values correctly
-J = 0;
-X_grad = zeros(size(X));
-Theta_grad = zeros(size(Theta));
+diff = (X * Theta' - Y) .* R;
+J = sum(sum(diff .^ 2)) / 2;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost function and gradient for collaborative
-%               filtering. Concretely, you should first implement the cost
-%               function (without regularization) and make sure it is
-%               matches our costs. After that, you should implement the 
-%               gradient and use the checkCostFunction routine to check
-%               that the gradient is correct. Finally, you should implement
-%               regularization.
-%
-% Notes: X - num_movies  x num_features matrix of movie features
-%        Theta - num_users  x num_features matrix of user features
-%        Y - num_movies x num_users matrix of user ratings of movies
-%        R - num_movies x num_users matrix, where R(i, j) = 1 if the 
-%            i-th movie was rated by the j-th user
-%
-% You should set the following variables correctly:
-%
-%        X_grad - num_movies x num_features matrix, containing the 
-%                 partial derivatives w.r.t. to each element of X
-%        Theta_grad - num_users x num_features matrix, containing the 
-%                     partial derivatives w.r.t. to each element of Theta
-%
+X_grad = (diff * Theta) + (lambda * X);
+Theta_grad = (diff' * X) + (lambda * Theta);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-% =============================================================
+% Regularization component
+J += (lambda / 2) * (sum(sum(Theta .^ 2)) + sum(sum(X .^ 2)));
 
 grad = [X_grad(:); Theta_grad(:)];
 
--- machine-learning/ex8/estimateGaussian.m
+++ machine-learning/ex8/estimateGaussian.m
@@ -11,26 +11,8 @@ function [mu sigma2] = estimateGaussian(X)
 [m, n] = size(X);
 
 % You should return these values correctly
-mu = zeros(n, 1);
-sigma2 = zeros(n, 1);
-
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the mean of the data and the variances
-%               In particular, mu(i) should contain the mean of
-%               the data for the i-th feature and sigma2(i)
-%               should contain variance of the i-th feature.
-%
-
-
-
-
-
-
-
-
-
-
-% =============================================================
-
+mu = sum(X) / m;
+diff = X - mu;
+sigma2 = sum(diff .* diff) / m;
 
 end
